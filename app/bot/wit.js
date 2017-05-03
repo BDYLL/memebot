08ec360d7ce99118d7a4529b9dcdf90372fefdab
Diego Toledo <toledo.diego09@gmail.com>
2017-05-03 00:42:59 -0500

    Implement Wit entities fetching

---
+++ app/bot/wit.js
@@ -0,0 +1,22 @@
+const { Wit, log } = require('node-wit');
+const client = new Wit({accessToken: process.env.WIT_TOKEN});
+
+const getCategory = async (message) => {
+  
+  // Make Wit API call.
+  const witResponse = await client.message(message, {})
+  const categories = witResponse.entities.intent || [];
+
+  // Get strongest category guess.
+  categories = categories.sort((a, b) => {
+    return a.confidence - b.confidence;
+  })
+
+  const category = categories.length != 0 ? 
+    categories[0].value : 
+    undefined;
+
+  return category;
+}
+
+module.exports = { getCategory };
